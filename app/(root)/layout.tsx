77b8b58dcb77d30d216d741bce7f2309843a11e5
elaifamouez <mouezelaifa@gmail.com>
2024-03-24 13:23:53 +0100

    add sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import Sidebar from '@/components/shared/Sidebar'
 import React from 'react'
 
 // This is for our home routes
@@ -5,7 +6,7 @@ import React from 'react'
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <main className='root'>
-       
+          <Sidebar />
 
             <div className='root-container'>
                 <div className='wrapper'>
