214570c8c597906ceed832c97120d3c9af9fa5e9
elaifamouez <mouezelaifa@gmail.com>
2024-03-29 21:30:38 +0100

    Update Middleware With New Credentiels

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-	publicRoutes: ["/", "/api/webhooks/clerk", "/api/webhooks/paypal"],
+	publicRoutes: ["/", "/api/webhooks/clerk", "/api/webhooks/approve-paypal-order", "/api/webhooks/create-paypal-order"],
 });
 
 export const config = {
